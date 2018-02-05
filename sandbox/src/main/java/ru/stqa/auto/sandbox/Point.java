7d67f67eaed37987fe004968116d191233e741ab
ozubk <sheraton.nnov@outlook.com>
2018-02-05 15:57:27 +0300

    Вычисление расстояния при помощи метода в классе Point

--- sandbox/src/main/java/ru/stqa/auto/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/auto/sandbox/Point.java
@@ -10,4 +10,9 @@ public class Point {
     this.x = x;
     this.y = y;
   }
+
+  public double distance(Point p) {
+
+    return Math.sqrt(Math.pow(p.x - this.x, 2) + Math.pow(p.y - this.y, 2));
+  }
 }
--- sandbox/src/main/java/ru/stqa/auto/sandbox/SecondProgram.java
+++ sandbox/src/main/java/ru/stqa/auto/sandbox/SecondProgram.java
@@ -6,17 +6,15 @@ public class SecondProgram {
 
     Point p1 = new Point(2.0, 1.0);
     Point p2 = new Point(-2.0, -3.0);
+    Point p3 = new Point(4.0, 3.75);
+    Point p4 = new Point (10.3, -5.53);
 
-    System.out.println("Расстояние между точками (" + p1.x + ',' + p1.y + ") и (" + p2.x + ',' + p2.y + ") = " + distance(p1, p2));
+
+    System.out.println("Расстояние между точками (" + p1.x + ',' + p1.y + ") и (" + p2.x + ',' + p2.y + ") = " + p1.distance(p2));
+    System.out.println("Расстояние между точками (" + p2.x + ',' + p2.y + ") и (" + p1.x + ',' + p1.y + ") = " + p2.distance(p1));
+    System.out.println("Расстояние между точками (" + p3.x + ',' + p3.y + ") и (" + p4.x + ',' + p4.y + ") = " + p4.distance(p3));
+    System.out.println("Расстояние между точками (" + p1.x + ',' + p1.y + ") и (" + p4.x + ',' + p4.y + ") = " + p4.distance(p1));
+    System.out.println("Расстояние между точками (" + p2.x + ',' + p2.y + ") и (" + p3.x + ',' + p3.y + ") = " + p3.distance(p2));
   }
 
-  public static double distance(Point p1, Point p2) {
-
-    return Math.sqrt(power(p2.x - p1.x) + power(p2.y - p1.y) );
-  }
-
-
-  public static double power(double x) {
-    return x * x;
-  }
 }
