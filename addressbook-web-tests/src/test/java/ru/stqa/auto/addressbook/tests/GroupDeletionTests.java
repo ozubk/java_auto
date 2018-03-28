fea442ee81b334443d71b4edc5ad68b6c32629ab
ozubk <sheraton.nnov@outlook.com>
2018-03-28 13:18:44 +0300

    Заменили наследование на делегирование

---
+++ addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/tests/GroupDeletionTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.auto.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+  @Test
+  public void testGroupDeletion() {
+    app.gotoGroupPage();
+    app.selectGroup();
+    app.deleteSelectedGroups();
+    app.returnToGroupPage();
+  }
+
+}
