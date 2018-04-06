39f42e7757a34b4df1f88d83d202b4335d551d03
ozubk <sheraton.nnov@outlook.com>
2018-04-06 12:39:19 +0300

    Revert "Создан базовый класс и GroupDeletionTests"
    This reverts commit 42f5c2541768b60be2fd8763a5ba2a8a507bc063.

--- addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/ContactCreationTests.java
@@ -17,11 +17,11 @@ public class ContactCreationTests {
   public void setUp() throws Exception {
     wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
     login("admin", "secret");
   }
 
   private void login(String username, String password) {
+    wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys(username);
