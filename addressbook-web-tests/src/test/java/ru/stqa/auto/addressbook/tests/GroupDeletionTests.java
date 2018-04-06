374a38583838f9344498bb88c17082ad7e113567
ozubk <sheraton.nnov@outlook.com>
2018-04-06 12:41:37 +0300

    Revert "Выделены специализированные классы-помощники"
    This reverts commit b390e07a459681826fa96594c520ede56c7deef8.

--- addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/tests/GroupDeletionTests.java
@@ -6,10 +6,10 @@ public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() {
-    app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().selectGroup();
-    app.getGroupHelper().deleteSelectedGroups();
-    app.getGroupHelper().returnToGroupPage();
+    app.gotoGroupPage();
+    app.selectGroup();
+    app.deleteSelectedGroups();
+    app.returnToGroupPage();
   }
 
 }
