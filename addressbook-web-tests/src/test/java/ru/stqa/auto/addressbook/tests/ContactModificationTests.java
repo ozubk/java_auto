c1f3dd30fcb30a7b64faea8e073f95ca82a68201
ozubk <sheraton.nnov@outlook.com>
2018-04-07 01:42:27 +0300

    Добавлен тест ContactModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.auto.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.auto.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase{
+
+  @Test
+  public void testContactModification() {
+    app.getNavigationHelper().gotoHomePage();
+    app.getContactHelper().selectContact();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillContactCreation(new ContactData("Test name1", "Test last name1", "1-111-111", "1-111-111-11-11", "test1@test.com"));
+    app.getContactHelper().submitContactModification();
+    app.getNavigationHelper().gotoHomePage();
+
+  }
+}
