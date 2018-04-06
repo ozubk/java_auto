676a7841f6caada975b659fbfd246c90d8b450ae
ozubk <sheraton.nnov@outlook.com>
2018-04-06 12:54:09 +0300

    возврат к состоянию до создания базового класса(т.к. в задании требуется одновременное преобразование классов для групп и контактов)

--- addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/GroupCreationTests.java
@@ -1,24 +1,15 @@
 package ru.stqa.auto.addressbook;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
 
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
-
-import static org.openqa.selenium.OutputType.*;
 
 public class GroupCreationTests {
   FirefoxDriver wd;
@@ -42,53 +33,6 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
-=======
-import org.testng.annotations.Test;
-
-public class GroupCreationTests extends TestBase {
->>>>>>> parent of fea442e... Заменили наследование на делегирование
-=======
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
-
-import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
-
-import static org.openqa.selenium.OutputType.*;
-
-public class GroupCreationTests {
-  FirefoxDriver wd;
-
-  @BeforeMethod
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.get("http://localhost/addressbook/group.php");
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.id("LoginForm")).click();
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-  }
->>>>>>> parent of 42f5c25... Создан базовый класс и GroupDeletionTests
 
   @Test
   public void testGroupCreation() {
@@ -100,10 +44,6 @@ public class GroupCreationTests {
     returnToGroupPage();
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of 42f5c25... Создан базовый класс и GroupDeletionTests
   private void gotoGroupPage() {
     wd.findElement(By.linkText("groups")).click();
   }
@@ -145,9 +85,5 @@ public class GroupCreationTests {
       return false;
     }
   }
-<<<<<<< HEAD
-=======
->>>>>>> parent of fea442e... Заменили наследование на делегирование
-=======
->>>>>>> parent of 42f5c25... Создан базовый класс и GroupDeletionTests
+
 }
