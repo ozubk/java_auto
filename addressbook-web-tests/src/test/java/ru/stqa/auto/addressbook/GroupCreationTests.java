d92396a2d8b90bb39317a29923bed6e1f610ad1f
ozubk <sheraton.nnov@outlook.com>
2018-04-06 12:43:07 +0300

    Revert "Создан базовый класс и GroupDeletionTests"
    This reverts commit 42f5c2541768b60be2fd8763a5ba2a8a507bc063.

--- addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/GroupCreationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.auto.addressbook;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
@@ -46,6 +47,48 @@ import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
 >>>>>>> parent of fea442e... Заменили наследование на делегирование
+=======
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+import static org.openqa.selenium.OutputType.*;
+
+public class GroupCreationTests {
+  FirefoxDriver wd;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    wd.get("http://localhost/addressbook/group.php");
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.id("LoginForm")).click();
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  }
+>>>>>>> parent of 42f5c25... Создан базовый класс и GroupDeletionTests
 
   @Test
   public void testGroupCreation() {
@@ -58,6 +101,9 @@ public class GroupCreationTests extends TestBase {
   }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 42f5c25... Создан базовый класс и GroupDeletionTests
   private void gotoGroupPage() {
     wd.findElement(By.linkText("groups")).click();
   }
@@ -99,6 +145,9 @@ public class GroupCreationTests extends TestBase {
       return false;
     }
   }
+<<<<<<< HEAD
 =======
 >>>>>>> parent of fea442e... Заменили наследование на делегирование
+=======
+>>>>>>> parent of 42f5c25... Создан базовый класс и GroupDeletionTests
 }
