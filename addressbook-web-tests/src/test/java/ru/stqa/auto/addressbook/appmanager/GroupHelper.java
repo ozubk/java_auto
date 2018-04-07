1189853f64dec3047890ebfc682a0e618a73ab73
ozubk <sheraton.nnov@outlook.com>
2018-04-07 12:37:36 +0300

    Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/appmanager/GroupHelper.java
@@ -1,12 +1,13 @@
 package ru.stqa.auto.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.auto.addressbook.model.GroupData;
 
 public class GroupHelper extends HelperBase {
 
-  public GroupHelper(FirefoxDriver wd) {
+  public GroupHelper(WebDriver wd) {
     super(wd);
   }
 
