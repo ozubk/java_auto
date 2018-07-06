7e86b0dc6ce0c44b8e4d54cb974651087880febc
ozubk <sheraton.nnov@outlook.com>
2018-07-06 10:40:05 +0300

    возобновление

--- addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/tests/ContactDeletionTests.java
@@ -7,6 +7,7 @@ public class ContactDeletionTests extends TestBase {
   @Test
   public void testContactDeletion() {
     app.getNavigationHelper().gotoHomePage();
+    if(app.getContactHelper().)
     app.getContactHelper().selectContact();
     app.getContactHelper().deleteSelectedContacts();
     app.getNavigationHelper().gotoHomePage();
